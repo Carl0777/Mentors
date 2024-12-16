3b3f9005f52180e2f545e7b8ac9f5badc1e7a8d4
Carl0777 <carlstipp@gmail.com>
2024-12-16 14:36:00 +0000

    lots of code

---
+++ test/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <?php
+$var = 25;
+$var2 = 28;
+if ($var2 > $var) {
+echo"greater than";}
+else
+{echo"less than";}
+
+<form action="welcome.php" method="POST">
+num1: <input type="integer" name="number1"><br>
+num2: <input type="integer" name="number2"><br>
+<input type="submit">
+    ?>
+
+
+</form>
+</body>
+</html>
